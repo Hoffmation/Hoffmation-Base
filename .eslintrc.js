4ec2772d2a2e8622c81352a5b6a36cff0291e11f
Thiemo <thiemo.hoffmann@web.de>
2021-10-08 08:56:42 +0200

    Add Configs

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    project: './tsconfig.json',
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    'plugin:prettier/recommended',
+  ],
+  plugins: [],
+  rules: {
+    'linebreak-style': 'off',
+    '@typescript-eslint/no-inferrable-types': [0],
+    '@typescript-eslint/adjacent-overload-signatures': [0],
+  },
+  overrides: [
+    {
+      files: ['*.test.ts'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+      },
+    },
+  ],
+};
