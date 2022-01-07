ce6bb504ac7fab5336c65250cb5bdfab4b5aec67
Thiemo Hoffmann <thiemo.hoffmann@web.de>
2022-01-07 20:45:37 +0100

    #85 Extend humidity possibilities

--- src/server/devices/iHumiditySensor.ts
+++ src/server/devices/iHumiditySensor.ts
@@ -2,4 +2,6 @@ import { IoBrokerBaseDevice } from './IoBrokerBaseDevice';
 
 export interface iHumiditySensor extends IoBrokerBaseDevice {
   humidity: number;
+
+  addHumidityCallback(pCallback: (pValue: number) => void): void;
 }
--- src/server/devices/zigbee/zigbeeSonoffTemp.ts
+++ src/server/devices/zigbee/zigbeeSonoffTemp.ts
@@ -6,6 +6,7 @@ import { DeviceType } from '../deviceType';
 
 export class ZigbeeSonoffTemp extends ZigbeeDevice implements iTemperaturSensor, iHumiditySensor {
   private _humidity: number = -99;
+  private _humidityCallbacks: ((pValue: number) => void)[] = [];
   private _temperatur: number = -99;
 
   public constructor(pInfo: DeviceInfo) {
@@ -16,7 +17,7 @@ export class ZigbeeSonoffTemp extends ZigbeeDevice implements iTemperaturSensor,
     super.update(idSplit, state, initial, true);
     switch (idSplit[3]) {
       case 'humidity':
-        this._humidity = state.val as number;
+        this.humidity = state.val as number;
         break;
       case 'temperatur':
         this._temperatur = state.val as number;
@@ -27,10 +28,26 @@ export class ZigbeeSonoffTemp extends ZigbeeDevice implements iTemperaturSensor,
   public get humidity(): number {
     return this._humidity;
   }
+
+  private set humidity(val: number) {
+    this._humidity = val;
+    for (const cb of this._humidityCallbacks) {
+      cb(val);
+    }
+  }
+
   public get iTemperatur(): number {
     return this._temperatur;
   }
+
   public get sTemperatur(): string {
     return `${this._temperatur}°C`;
+  }
+
+  public addHumidityCallback(pCallback: (pValue: number) => void): void {
+    this._humidityCallbacks.push(pCallback);
+    if (this._humidity > 0) {
+      pCallback(this._humidity);
+    }
   }
 }
