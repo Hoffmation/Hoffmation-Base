230e4ac2fbc9ea0c546effb9fad158655d7b436b
Thiemo Hoffmann <20605452+theimo1221@users.noreply.github.com>
2022-04-09 11:46:33 +0200

    Update packages and fix lint issues

--- test/services/time-callback-service.test.ts
+++ test/services/time-callback-service.test.ts
@@ -1,4 +1,4 @@
-import { SunTimeOffsets, TimeCallback, TimeCallbackService, TimeCallbackType } from "../../src";
+import { SunTimeOffsets, TimeCallback, TimeCallbackService, TimeCallbackType } from '../../src';
 
 describe('TimeCallbackService', () => {
   it('next Maximum Time is Today and correct', async () => {
@@ -23,44 +23,104 @@ describe('TimeCallbackService', () => {
   });
   it('Time Callback for Rollo Sunrise is calculated correct', async () => {
     const offset: SunTimeOffsets = new SunTimeOffsets(0, 0, 6, 30, 22, 30);
-    const cb: TimeCallback = new TimeCallback('', TimeCallbackType.Sunrise, () => {}, 0, undefined, undefined, offset);
+    const cb: TimeCallback = new TimeCallback(
+      '',
+      TimeCallbackType.Sunrise,
+      () => {
+        /*Nothing*/
+      },
+      0,
+      undefined,
+      undefined,
+      offset,
+    );
     TimeCallbackService.updateSunRise(new Date(1649543400000));
     cb.recalcNextToDo(new Date(1649529000000));
-    expect(cb.nextToDo!.getTime()).toBe(1649566197828);
+    expect(cb.nextToDo?.getTime()).toBe(1649566197828);
   });
   it('Time Callback for Rollo Sunrise respects offset', async () => {
     const offset: SunTimeOffsets = new SunTimeOffsets(10, 0, 6, 30, 22, 30);
-    const cb: TimeCallback = new TimeCallback('', TimeCallbackType.Sunrise, () => {}, offset.sunrise, undefined, undefined, offset);
+    const cb: TimeCallback = new TimeCallback(
+      '',
+      TimeCallbackType.Sunrise,
+      () => {
+        /*Nothing*/
+      },
+      offset.sunrise,
+      undefined,
+      undefined,
+      offset,
+    );
     TimeCallbackService.updateSunRise(new Date(1649543400000));
     cb.recalcNextToDo(new Date(1649529000000));
-    expect(cb.nextToDo!.getTime()).toBe(1649566797828);
+    expect(cb.nextToDo?.getTime()).toBe(1649566797828);
   });
   it('Time Callback for Rollo Sunrise respects min hours', async () => {
     const offset: SunTimeOffsets = new SunTimeOffsets(0, 0, 7, 30, 22, 30);
-    const cb: TimeCallback = new TimeCallback('', TimeCallbackType.Sunrise, () => {}, 0, undefined, undefined, offset);
+    const cb: TimeCallback = new TimeCallback(
+      '',
+      TimeCallbackType.Sunrise,
+      () => {
+        /*Nothing*/
+      },
+      0,
+      undefined,
+      undefined,
+      offset,
+    );
     TimeCallbackService.updateSunRise(new Date(1649543400000));
     cb.recalcNextToDo(new Date(1649529000000));
-    expect(cb.nextToDo!.getTime()).toBe(1649568600000);
+    expect(cb.nextToDo?.getTime()).toBe(1649568600000);
   });
   it('Time Callback for Rollo Sunset is calculated correct', async () => {
     const offset: SunTimeOffsets = new SunTimeOffsets(0, 0, 6, 30, 22, 30);
-    const cb: TimeCallback = new TimeCallback('', TimeCallbackType.SunSet, () => {}, 0, undefined, undefined, offset);
+    const cb: TimeCallback = new TimeCallback(
+      '',
+      TimeCallbackType.SunSet,
+      () => {
+        /*Nothing*/
+      },
+      0,
+      undefined,
+      undefined,
+      offset,
+    );
     TimeCallbackService.updateSunSet(new Date(1649543400000));
     cb.recalcNextToDo(new Date(1649529000000));
-    expect(cb.nextToDo!.getTime()).toBe(1649614660829);
+    expect(cb.nextToDo?.getTime()).toBe(1649614660829);
   });
   it('Time Callback for Rollo Sunset respects offset', async () => {
     const offset: SunTimeOffsets = new SunTimeOffsets(0, -10, 6, 30, 22, 30);
-    const cb: TimeCallback = new TimeCallback('', TimeCallbackType.SunSet, () => {}, offset.sunset, undefined, undefined, offset);
+    const cb: TimeCallback = new TimeCallback(
+      '',
+      TimeCallbackType.SunSet,
+      () => {
+        /*Nothing*/
+      },
+      offset.sunset,
+      undefined,
+      undefined,
+      offset,
+    );
     TimeCallbackService.updateSunSet(new Date(1649543400000));
     cb.recalcNextToDo(new Date(1649529000000));
-    expect(cb.nextToDo!.getTime()).toBe(1649614060829);
+    expect(cb.nextToDo?.getTime()).toBe(1649614060829);
   });
   it('Time Callback for Rollo Sunset respects maximum Hours', async () => {
     const offset: SunTimeOffsets = new SunTimeOffsets(0, 0, 6, 30, 20, 0);
-    const cb: TimeCallback = new TimeCallback('', TimeCallbackType.SunSet, () => {}, 0, undefined, undefined, offset);
+    const cb: TimeCallback = new TimeCallback(
+      '',
+      TimeCallbackType.SunSet,
+      () => {
+        /*Nothing*/
+      },
+      0,
+      undefined,
+      undefined,
+      offset,
+    );
     TimeCallbackService.updateSunSet(new Date(1649602800000));
     cb.recalcNextToDo(new Date(1649602800000));
-    expect(cb.nextToDo!.getTime()).toBe(1649613600000);
+    expect(cb.nextToDo?.getTime()).toBe(1649613600000);
   });
 });
