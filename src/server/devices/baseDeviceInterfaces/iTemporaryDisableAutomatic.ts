1edd25b52b31734f9f6b443f6bdda7d6f9e5c896
Thiemo Hoffmann <20605452+theimo1221@users.noreply.github.com>
2022-11-16 21:41:02 +0100

    #616 Improve interface

--- src/server/devices/baseDeviceInterfaces/iTemporaryDisableAutomatic.ts
+++ src/server/devices/baseDeviceInterfaces/iTemporaryDisableAutomatic.ts
@@ -2,6 +2,8 @@ import { CollisionSolving } from '../../../models';
 
 export interface iTemporaryDisableAutomatic {
   readonly automaticBlockedUntil: Date;
+  readonly automaticBlockActive: boolean;
+  readonly restoreAutomaticStateTimeout: NodeJS.Timeout | null;
 
   disableAutomatic(duration: number, onCollideAction?: CollisionSolving): void;
 
