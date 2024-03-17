974b62bf42af32121088830788cfed908511752f
Thiemo Hoffmann <20605452+theimo1221@users.noreply.github.com>
2024-03-17 22:45:23 +0100

    Fix unit test

--- test/devices/commands.test.ts
+++ test/devices/commands.test.ts
@@ -1,13 +1,13 @@
-import { ActuatorWriteStateToDeviceCommand, CommandSource, LampSetLightCommand } from '../../src';
+import { ActuatorSetStateCommand, CommandSource, LampSetLightCommand } from '../../src';
 
 describe('Commands', () => {
   it('Should print a proper reason Tree', () => {
     const c1 = new LampSetLightCommand(CommandSource.Force, false, 'Testreason Lamp');
-    const c2 = new ActuatorWriteStateToDeviceCommand(c1, true, 'Testreason Actuator');
+    const c2 = new ActuatorSetStateCommand(c1, true, 'Testreason Actuator');
 
     const result = c2.logMessage;
     expect(result).toBe(
-      'Actuator writeStateToDevice(true) for reason: CommandType("Force") stack => LampSetLightCommand("Testreason Lamp") -> ActuatorWriteStateToDeviceCommand("Testreason Actuator")',
+      'Actuator setState to true for reason: CommandType("Force") stack => LampSetLightCommand("Testreason Lamp") -> ActuatorSetStateCommand("Testreason Actuator")',
     );
   });
 });
