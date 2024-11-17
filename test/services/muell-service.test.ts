6089d9fdea8254199f76d5d50faffd80c6673d07
Thiemo Hoffmann <20605452+theimo1221@users.noreply.github.com>
2024-11-17 17:22:36 +0100

    Fix Muell-Service Test by readding an example street which has all 4 bins to be emptied

--- test/services/muell-service.test.ts
+++ test/services/muell-service.test.ts
@@ -1,16 +1,16 @@
 import { MuellService, ServerLogService } from '../../src';
 
 describe('MuellService', () => {
-  jest.setTimeout(15000);
+  jest.setTimeout(10000);
   ServerLogService.settings.logLevel = -1;
   it('Should download calendar and initialize objects', async () => {
     MuellService.intialize(
       {
-        calendarURL: 'https://gelsendienste.abisapp.de/abfuhrkalender?format=ical&street=CDB7055D&number=1',
+        calendarURL: 'https://gelsendienste.abisapp.de/abfuhrkalender?format=ical&street=40E7634F&number=1',
       },
       undefined,
     );
-    await new Promise((r) => setTimeout(r, 5000));
+    await new Promise((r) => setTimeout(r, 8000));
     if (MuellService.loadingPending) {
       return;
     }
